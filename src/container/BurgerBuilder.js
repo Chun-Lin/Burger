b06a04d88a60a89c1f6547b5a96ff75db1fd45e4
Chun-Lin <wulin40063@gmail.com>
2018-05-07 16:55:33 +0800

    feat: route to '/checkout' when click continue button on the modal

--- src/container/BurgerBuilder.js
+++ src/container/BurgerBuilder.js
@@ -72,32 +72,33 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true })
+    // this.setState({ loading: true })
 
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'ChunLin Wu',
-        address: {
-          street: 'Teststreet 1',
-          zipCode: '41351',
-          country: 'Taiwan',
-        },
-        email: 'test@test.com',
-      },
-      deliveryMethod: 'fastest',
-    }
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'ChunLin Wu',
+    //     address: {
+    //       street: 'Teststreet 1',
+    //       zipCode: '41351',
+    //       country: 'Taiwan',
+    //     },
+    //     email: 'test@test.com',
+    //   },
+    //   deliveryMethod: 'fastest',
+    // }
 
-    axios
-      .post('/orders.json', order)
-      .then(response => {
-        console.log(response)
-        this.setState({ purchasing: false, loading: false })
-      })
-      .catch(error => {
-        console.log(error)
-      })
+    // axios
+    //   .post('/orders.json', order)
+    //   .then(response => {
+    //     console.log(response)
+    //     this.setState({ purchasing: false, loading: false })
+    //   })
+    //   .catch(error => {
+    //     console.log(error)
+    //   })
+    this.props.history.push('/checkout')
   }
 
   addTotalPrice = type => {
