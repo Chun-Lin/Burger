30f9db771504cc2f0218cef2c1f0fdf482638095
Chun-Lin <wulin40063@gmail.com>
2018-05-07 17:14:00 +0800

    feat: add path when clicking Cancel or Contiune button in checkout page

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -14,15 +14,23 @@ const StyledCheckoutSummary = styled.div`
   }
 `
 
-const CheckoutSummary = ({ ingredients }) => {
+const CheckoutSummary = ({
+  ingredients,
+  checkoutCancelled,
+  checkoutContinued,
+}) => {
   return (
     <StyledCheckoutSummary>
       <h1>We hope it tastes well!</h1>
       <div style={{ width: '100%', margin: 'auto' }}>
         <Burger ingredients={ingredients} />
       </div>
-      <Button btnType="Danger">CANCEL</Button>
-      <Button btnType="Success">CONTINUE</Button>
+      <Button btnType="danger" onClick={checkoutCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="success" onClick={checkoutContinued}>
+        CONTINUE
+      </Button>
     </StyledCheckoutSummary>
   )
 }
--- src/container/Checkout/Checkout.js
+++ src/container/Checkout/Checkout.js
@@ -5,20 +5,32 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 class Checkout extends Component {
   state = {
     ingredients: {
-        salad: 1,
-        meat: 1,
-        cheese: 1,
-        bacon: 1
-    }
-}
+      salad: 1,
+      meat: 1,
+      cheese: 1,
+      bacon: 1,
+    },
+  }
 
-render() {
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack()
+  }
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace('/checkout/contact-data')
+  }
+
+  render() {
     return (
-        <div>
-            <CheckoutSummary ingredients={this.state.ingredients}/>
-        </div>
-    );
-}
+      <div>
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}
+        />
+      </div>
+    )
+  }
 }
 
 export default Checkout
