22e4a349ac743ec4881ed1d9db3e8f0b5b4c735e
Chun-Lin <wulin40063@gmail.com>
2018-05-08 17:05:31 +0800

    feat: add nested route to route ContactData comopnent inside Checkout page

--- src/container/Checkout/ContactData/ContactData.js
+++ src/container/Checkout/ContactData/ContactData.js
@@ -1,31 +1,49 @@
-// import React, {Component, Fragment} from 'react'
+import React, { Component, Fragment } from 'react'
+import styled from 'styled-components'
 
-// import Button from '../../../components/UI/Button/Button'
+import Button from '../../../components/UI/Button/Button'
 
-// class ContactData extends Comopnent {
-//   state = {
-//     name: '',
-//     email: '',
-//     address: {
-//         street: '',
-//         postalCode: ''
-//     },
-//     loading: false
-//   }
+const StyledContactData = styled.div`
+  margin: 20px auto;
+  width: 80%;
+  text-align: center;
+  box-shadow: 0 2px 3px #ccc;
+  border: 1px solid #eee;
 
-//   render(){
-//     return (
-//       <Fragment>
-//         <h4>Enter Your Contact Data</h4>
-//         <form>
-//           <input type="text" name="name" placeholder="Your Name"/>
-//           <input type="email" name="email" placeholder="Your Email"/>
-//           <input type="text" name="street" placeholder="Street"/>
-//           <input type="text" name="postal" placeholder="Postal Code"/>
-//           <Button btnType="success">ORDER</Button>
-//         </form>
-//       </Fragment>
-//     )
-//   }
+  @media(min-width: 600px){
+    width: 500px;
+  }
+`
 
-// }
+const StyledInput = styled.input`
+  display: block;
+`
+
+class ContactData extends Component {
+  state = {
+    name: '',
+    email: '',
+    address: {
+      street: '',
+      postalCode: '',
+    },
+    loading: false,
+  }
+
+  render() {
+    return (
+      <StyledContactData>
+        <h4>Enter Your Contact Data</h4>
+        <form>
+          <StyledInput type="text" name="name" placeholder="Your Name" />
+          <StyledInput type="email" name="email" placeholder="Your Email" />
+          <StyledInput type="text" name="street" placeholder="Street" />
+          <StyledInput type="text" name="postal" placeholder="Postal Code" />
+          <Button btnType="success">ORDER</Button>
+        </form>
+      </StyledContactData>
+    )
+  }
+}
+
+export default ContactData
