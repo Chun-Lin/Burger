122f114601599273141db1db20af40cdf5e02b93
Chun-Lin <wulin40063@gmail.com>
2018-07-04 16:51:42 +0800

    fix: move redux devtools to development env

--- src/index.js
+++ src/index.js
@@ -10,7 +10,10 @@ import burgerBuilderReducer from './container/store/reducers/burgerBuilder'
 import orderReducer from './container/store/reducers/order'
 import authReducer from './container/store/reducers/auth'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers =
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
